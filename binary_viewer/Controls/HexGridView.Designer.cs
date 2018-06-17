01f4abc57ef31b9923898fc0df00443c9a76156d
RythBlade <redeyescf@hotmail.com>
2018-06-17 19:09:42 +0100

    Integrated BE Hexbox
    Swapped the hex grid view out for the Be.HexBox implementation

--- binary_viewer/Controls/HexGridView.Designer.cs
+++ binary_viewer/Controls/HexGridView.Designer.cs
@@ -6,6 +6,7 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private Be.Windows.Forms.HexBox hexBox = null;
 
         /// <summary> 
         /// Clean up any resources being used.
@@ -28,83 +29,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer = new System.Windows.Forms.SplitContainer();
-            this.hexView = new System.Windows.Forms.DataGridView();
-            this.dataView = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
-            this.splitContainer.Panel1.SuspendLayout();
-            this.splitContainer.Panel2.SuspendLayout();
-            this.splitContainer.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.hexView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.hexBox = new Be.Windows.Forms.HexBox();
             this.SuspendLayout();
             // 
-            // splitContainer
+            // hexBox
             // 
-            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer.IsSplitterFixed = true;
-            this.splitContainer.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer.Name = "splitContainer";
-            // 
-            // splitContainer.Panel1
-            // 
-            this.splitContainer.Panel1.Controls.Add(this.hexView);
-            // 
-            // splitContainer.Panel2
-            // 
-            this.splitContainer.Panel2.Controls.Add(this.dataView);
-            this.splitContainer.Size = new System.Drawing.Size(219, 182);
-            this.splitContainer.SplitterDistance = 110;
-            this.splitContainer.TabIndex = 0;
-            // 
-            // hexView
-            // 
-            this.hexView.AllowUserToAddRows = false;
-            this.hexView.AllowUserToDeleteRows = false;
-            this.hexView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.hexView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.hexView.Location = new System.Drawing.Point(0, 0);
-            this.hexView.Name = "hexView";
-            this.hexView.RowHeadersWidth = 50;
-            this.hexView.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.hexView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.hexView.Size = new System.Drawing.Size(110, 182);
-            this.hexView.TabIndex = 0;
-            // 
-            // dataView
-            // 
-            this.dataView.AllowUserToAddRows = false;
-            this.dataView.AllowUserToDeleteRows = false;
-            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.dataView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataView.Location = new System.Drawing.Point(0, 0);
-            this.dataView.Name = "dataView";
-            this.dataView.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.dataView.Size = new System.Drawing.Size(105, 182);
-            this.dataView.TabIndex = 0;
+            this.hexBox.ColumnInfoVisible = true;
+            this.hexBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hexBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.hexBox.GroupSeparatorVisible = true;
+            this.hexBox.Location = new System.Drawing.Point(0, 0);
+            this.hexBox.Name = "hexBox";
+            this.hexBox.ShadowSelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(60)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
+            this.hexBox.Size = new System.Drawing.Size(328, 280);
+            this.hexBox.StringViewVisible = true;
+            this.hexBox.TabIndex = 0;
+            this.hexBox.VScrollBarVisible = true;
             // 
             // HexGridView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.hexBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "HexGridView";
-            this.Size = new System.Drawing.Size(219, 182);
-            this.splitContainer.Panel1.ResumeLayout(false);
-            this.splitContainer.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
-            this.splitContainer.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.hexView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.Size = new System.Drawing.Size(328, 280);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.SplitContainer splitContainer;
-        private System.Windows.Forms.DataGridView hexView;
-        private System.Windows.Forms.DataGridView dataView;
     }
 }
