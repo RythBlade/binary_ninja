500f3a93a82c719bf877d52f07257d1dcb112a87
RythBlade <redeyescf@hotmail.com>
2019-02-16 09:58:03 +0000

    Moved the loading dialogue into the dialogues folder and adjusted it's namespace appropriatley.

---
+++ binary_viewer/Dialogues/LoadingDialogue.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace binary_viewer.Dialogues
+{
+    partial class LoadingDialogue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.loadingLabel = new System.Windows.Forms.Label();
+            this.screenTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 25);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(365, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 0;
+            this.progressBar.UseWaitCursor = true;
+            // 
+            // loadingLabel
+            // 
+            this.loadingLabel.AutoSize = true;
+            this.loadingLabel.Location = new System.Drawing.Point(12, 9);
+            this.loadingLabel.Name = "loadingLabel";
+            this.loadingLabel.Size = new System.Drawing.Size(54, 13);
+            this.loadingLabel.TabIndex = 1;
+            this.loadingLabel.Text = "Loading...";
+            this.loadingLabel.UseWaitCursor = true;
+            // 
+            // screenTimer
+            // 
+            this.screenTimer.Tick += new System.EventHandler(this.cooldownTimer_Tick);
+            // 
+            // LoadingDialogue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 67);
+            this.ControlBox = false;
+            this.Controls.Add(this.loadingLabel);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingDialogue";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "LoadingDialogue";
+            this.TopMost = true;
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label loadingLabel;
+        private System.Windows.Forms.Timer screenTimer;
+    }
+}
---
+++ binary_viewer/Dialogues/LoadingDialogue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace binary_viewer.Dialogues
+{
+    public partial class LoadingDialogue : Form
+    {
+        public int CooldownTime { get; set; }
+        
+        public int TimerInterval
+        {
+            get { return screenTimer.Interval; }
+            set { screenTimer.Interval = value; }
+        }
+
+        public string LoadingText
+        {
+            get { return loadingLabel.Text; }
+            set { loadingLabel.Text = value; this.Invalidate(); }
+        }
+
+        private bool m_closeTriggered = false;
+        private int m_currentTime = 0;
+
+        public LoadingDialogue()
+        {
+            InitializeComponent();
+
+            CooldownTime = 200;
+        }
+
+        public void TriggerCloseLoading()
+        {
+            m_closeTriggered = true;
+            m_currentTime = 0;
+            screenTimer.Start();
+        }
+
+        public void CancelCloseLoading()
+        {
+            screenTimer.Stop();
+            m_closeTriggered = false;
+            m_currentTime = 0;
+        }
+
+        private void cooldownTimer_Tick(object sender, EventArgs e)
+        {
+            if( m_closeTriggered )
+            {
+                m_currentTime += screenTimer.Interval;
+
+                if(m_currentTime >= CooldownTime)
+                {
+                    // we're done with the loading dialog, so close it
+                    Close();
+                }
+            }
+            else
+            {
+                m_currentTime = 0;
+            }
+        }
+    }
+}
